7d0d98f05a9211dd62926a15fc9b7e0e812f4651
JubairAnjumRony <rony.ict.iu@gmail.com>
2024-05-16 23:52:38 +0600

    16-05-24

---
+++ .vscode/1760A.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b,c;
+        cin>>a>>b>>c;
+       
+        if(a>b){
+            swap(a,b);
+
+        }
+       
+        if(a>c){
+            swap(a,c);
+        }
+         if(b>c){
+            swap(b,c);
+        }
+        cout<<b<<endl;
+    }
+    return 0;
+}
