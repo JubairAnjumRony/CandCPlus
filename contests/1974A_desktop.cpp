6621c3c204d7080358129a0cc1e5e793906e0a53
JubairAnjumRony <rony.ict.iu@gmail.com>
2024-05-21 23:47:49 +0600

    wrong ans on test2

--- contests/1974A_desktop.cpp
+++ contests/1974A_desktop.cpp
@@ -7,22 +7,25 @@ int main(){
     while(t--){
         int x,y;
         cin>>x>>y;
-        int screen=0;
+       
         int xspace=x*1;
        int yspace=y*4;
-      int  freespace=15-yspace;
+       int screen=1;
+       if(x==0 && y==0){
+        screen=0;
+       }
+       if(y>2){
+        screen=yspace/8;
+       }
+      
+      int totalspace=screen*15;
+      if(xspace+yspace>totalspace  || y>2&&yspace%8!=0){
+        screen=screen+1;
+    
+      }
+      cout<<screen<<endl;
 
-    for(int i=1;i<30;i++) {
-        if(y<=2 && x<=freespace){
-            screen=1;
-            break;
-        }
-        if(y>2 || x>freespace){
-            y=y-4*2;
-        screen ++;
-        } 
-    }
-        cout<<screen<<endl;
+      
     }
     
     return 0;
